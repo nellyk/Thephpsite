5007aa6d715d99abc032401e0f55149238c6c749
nellyk <nellykiboi@gmail.com>
2015-03-19 00:45:10 +0300

    adding index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>PHP Example</title>
+	
+		<link rel="stylesheet"type="text/css"href="style.css">
+</head>
+<body>
+<div id="navigation">
+<ul>
+<li><a href="#">Home Page</a></li>
+<li><a href="#">About Us</a></li>
+<li><a href="#">Table</a></li>
+<li><a href="#">Contact Form</a></li>
+</ul>
+</div>
+</body>
+</html>
